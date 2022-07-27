4dd5996b73b70fd2d3e5fd753623cc01507e9038
Myllena Cruz <myllena.cruz@tcsindustrial.com.br>
2022-07-27 19:58:45 -0300

    add mock de usuario

---
+++ src/domains/users/mocks/user.ts
@@ -0,0 +1,7 @@
+import { ICreateUserDTO } from "@domains/users/dtos/ICreateUserDTO";
+
+export const comumUser: ICreateUserDTO = {
+	name: "Jane Roe",
+	email: "janeroe@email.com",
+	password: "janeroe"
+}
